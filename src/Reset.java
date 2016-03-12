f13d192940989894acae24f62a2e6b3e6b0ad518
jmcrawford45 <jared13@stanford.edu>
2016-03-11 17:38:53 -0800

    Added files via upload

---
+++ src/Reset.java
@@ -0,0 +1,38 @@
+package listeners;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Reset {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final String MYSQL_USERNAME = "ccs108jared13";
+		final String MYSQL_PASSWORD = "2zVt6gGlm5GtBhMf";
+		final String MYSQL_DATABASE_SERVER = "mysql-user.stanford.edu";
+		final String MYSQL_DATABASE_NAME = "c_cs108_jared13";
+		Connection con = null;
+    	try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://" + 
+            MYSQL_DATABASE_SERVER, MYSQL_USERNAME , MYSQL_PASSWORD);
+            Statement stmt = con.createStatement();
+            stmt.executeQuery("USE " + MYSQL_DATABASE_NAME);
+            stmt.executeUpdate("DROP TABLE IF EXISTS users;");
+            stmt.executeUpdate("CREATE TABLE users(uid INT, id varchar(255), user BLOB);");
+            stmt.executeUpdate("DROP TABLE IF EXISTS stats;");
+            stmt.executeUpdate("CREATE TABLE stats(stats BLOB);");
+    		} catch (SQLException e) {
+            e.printStackTrace();
+            }
+            catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            }
+
+	}
+
+}
